bed8284186592ef80ec9b65081436491cb54acef
Maurits Meijer <meijer20@gmail.com>
2013-07-03 11:07:06 +0200

    refactored the whole project

--- web.js
+++ web.js
@@ -1,7 +1,28 @@
+var express = require('express');
+var app = express();
+var webcommand=require('./lib/index.js');
+
 var port = process.env.PORT || 8000;
 
-var webcommand=require('./index.js');
+var commandServer=webcommand.createCommandServer();
 
-webcommand.createCommandServer('sort').listen(port);
-console.log('Server running at http://localhost:'+port+'/');
-//sort, awk, sed, grep, uniq, head, tail, cut, fmt, wc
+app.get('/getCommands', function(req,res) {
+	  res.send(JSON.stringify(commandServer.getCommandList()));
+	});
+
+app.get('/', function(req,res) {
+	  res.sendfile('index.html');
+	});
+
+app.post('/index.html', function(req,res){
+	 req.on('error', function(error){
+		    console.log("req Error: "+error);
+		});
+	 res.on('error', function(error){
+		    console.log("res Error: "+error);
+		});
+	commandServer.webCommand('sort',req.query.args.split(' '), req, res);
+});
+
+app.listen(port);
+console.log('Listening on port: '+port);
