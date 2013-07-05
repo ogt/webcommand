c5fb5c2da8241a4a2d3874518bbb08680b9ccbd9
Maurits Meijer <meijer20@gmail.com>
2013-07-05 09:32:02 +0200

    changed tests to use err streams

--- index.js
+++ index.js
@@ -7,35 +7,49 @@ createCommandServer = function(cmdList) {
 	}
 
 	function webCommand (cmd, args, ins, outs) {
+		var result= require('event-stream').through();
 		if(cmdList.indexOf(cmd)===-1){
-			outs.emit('error', new Error('Illegal command'));
-			outs.end();
+			setTimeout(function(){
+				result.emit('error', new Error('Illegal command'));
+				result.end();
+		},10);
 		}else{
-
+			
 			var proc=spawn(cmd, args);
 			var procStream = child(proc);
 			proc.stderr.on('data', function (data) {
-				outs.emit('error', new Error('Execution error'));
-				outs.end();
+				//outs.emit('error', new Error('Execution error'));
+				//outs.end();
 			});
-			/*procStream.on('exit', function(msg){
+			proc.on('exit', function(msg){
 				console.log('exiting now!');
 				console.log(msg);
 				if(msg!==0){
-					outs.emit('error', new Error('Spawn error'));
-					outs.end();
+					result.emit('error', new Error('Spawn error'));
 				}
-			});*/
+				result.end();
+			});
 
 			proc.on('uncaughtException', function (data) {
-				outs.emit('error', new Error('Spawn error'));
-				outs.end();
 			});
+			procStream.on('uncaughtException', function (data) {
+			});
+			outs.on('uncaughtException', function (data) {
+			});
+			ins.on('uncaughtException', function (data) {
+			});
+			proc.on('error', function (data) {
+			});
+			procStream.on('error', function (data) {
+			});
+			/*outs.on('error', function (data) {
+			});
+			ins.on('error', function (data) {
+			});*/
 			ins.pipe(procStream )
 			.pipe(outs);
-
-			
 		}
+		return result;
 	};
 
 	function getCommandList(){
