651c4aca8457dc1b40a46419851af251aaa5d49b
Ubuntu <ubuntu@ip-10-204-131-226.ec2.internal>
2013-07-05 07:33:21 +0000

    working on ec2

--- index.js
+++ index.js
@@ -11,16 +11,34 @@ createCommandServer = function(cmdList) {
 			outs.emit('error', new Error('Illegal command'));
 			outs.end();
 		}else{
-			var proc=spawn(cmd, args);
+	    var proc=spawn(cmd, args);
 			var procStream = child(proc);
-			proc.stderr.on('data', function (data) {
+/*		    }catch(e){
+                        outs.emit('error', new Error('Execution error'));
+                        outs.end();
+   
+}
+*/			proc.stderr.on('data', function (data) {
 				outs.emit('error', new Error('Execution error'));
 				outs.end();
 			});
-			proc.on('error', function (data) {
+/*		    proc.on('exit', function(msg){
+			if(msg!==0){
+			    outs.emit('error', new Error('lala'));
+			    outs.end();
+			}
+})*/
+
+			proc.on('uncaughtException', function (data) {
 				outs.emit('error', new Error('Spawn error'));
 				outs.end();
 			});
+		    /*all.on('error', function(){
+
+                        outs.emit('error', new Error('Spawn error'));
+                        outs.end();
+  
+})*/
 
 			var all=ins.pipe(procStream )
 			.pipe(outs);
